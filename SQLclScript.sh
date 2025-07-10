3676891efb7ecd347549ba95bdb7600491cd756f
Phil Cannata <phil.cannata@oracle.com>
2025-07-10 07:49:07 -0500

    On the Blog tab, when I retrieve a blog from the database, the clob that displays i the main blog window is truncated. This might be due to SQLclScript.sh. It uses SQLcl, which might be doing the truncation. Please investigate.
    Excellent! The CLOB content is now being retrieved in full. The SQLcl settings I added have fixed the truncation issue:
    
    •  set long 10000000 - Increases the maximum length for LONG and CLOB data types
    •  set pagesize 0 - Removes pagination
    •  set linesize 32767 - Sets maximum line size
    •  set wrap off - Prevents line wrapping
    •  set trimout on - Trims trailing spaces
    •  set trimspool on - Trims trailing spaces in spooled output

--- SQLclScript.sh
+++ SQLclScript.sh
@@ -7,6 +7,12 @@ fi
 sql -S RAGUSER/WelcomeRAG123###@129.213.106.172/RAG23ai_PDB1.sub08201532330.philfnvcn.oraclevcn.com <<!
 SET SQLFORMAT JSON-FORMATTED
 set feedback off
+set long 10000000
+set pagesize 0
+set linesize 32767
+set wrap off
+set trimout on
+set trimspool on
 $1;
 commit;
 !
